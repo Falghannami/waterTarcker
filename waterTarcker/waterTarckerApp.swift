fa412328bdd9ee62dfa9f3d10dadb3bde7eceb32
Falghannami <falghannami24@twq.idserve.net>
2024-09-29 08:52:49 +0300

    Initial Commit

---
+++ waterTarcker/waterTarckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterTarckerApp.swift
+//  waterTarcker
+//
+//  Created by فالحه الغنامي on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct waterTarckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ waterTarckerTests/waterTarckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  waterTarckerTests.swift
+//  waterTarckerTests
+//
+//  Created by فالحه الغنامي on 26/03/1446 AH.
+//
+
+import Testing
+@testable import waterTarcker
+
+struct waterTarckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
